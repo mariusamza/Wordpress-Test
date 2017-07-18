13badbdd774f25357b6fbdf2be983a59928b4086
Unknown <bratzaharia@gmail.com>
2017-07-18 15:40:49 +0300

    renamed plugin name
    to new name

--- wp-content/plugins/my-widget/my-widget.php
+++ wp-content/plugins/my-widget/my-widget.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-Plugin Name: My Widget - Please don't use ^_^
+Plugin Name: Mini Widget - Please don't use ^_^
 Plugin URI: https://swissacademy.eu
 Description: This is a widget plugin that describes how to kill wordpress
 Author: CtrlAltDelicious
