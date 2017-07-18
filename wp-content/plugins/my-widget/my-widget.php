c49f4301b86c36f56d8a7c013cdf92b5ef60a0bc
Unknown <bratzaharia@gmail.com>
2017-07-18 15:46:20 +0300

    New banner test
    Is a new test banner to test a merged file

--- wp-content/plugins/my-widget/my-widget.php
+++ wp-content/plugins/my-widget/my-widget.php
@@ -10,3 +10,22 @@ Author URI: https://swissacademy.eu
 
 
 
+add_shortcode('test2_banner','randomTestBanner');
+
+/*
+add_action('admin_notices','notificari');
+add_filter('widget_text','do_shortcode');
+add_filter('widget_title','modificaTitlu');
+*/
+
+
+function randomTestBanner() {
+    $bannere = array(
+    'http://placehold.it/350x80/ff0000/ffffff',
+    'http://placehold.it/350x80/00ff00/ff30ee'
+    );
+    
+    $banner_selectat = $bannere[rand(0,3)];
+    
+    echo "<img src='$banner_selectat' >";    
+}
