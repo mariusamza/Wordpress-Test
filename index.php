8bce4d43149b152ab647124ec0170a89fc6ce355
Marius Amza <mariusamza@gmail.com>
2021-11-24 19:49:09 +0200

    Updated description

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: SWA Test Plugin
 Plugin URI: https://swissacademy.eu
 Description: This is a wordpress implementation of a demo plugin
-Author: Marius Amza
-Version: 0.1
+Author: Marius Amza / Swiss WebAcademy
+Version: 0.2
 Author URI: https://swissacademy.eu
 */
