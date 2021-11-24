580473ef48c98a2f9bf8e96b0dd1b97f1e3bc252
Marius Amza <mariusamza@gmail.com>
2021-11-24 19:43:40 +0200

    Initialized WordPress plugin

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: SWA Test Plugin
+Plugin URI: https://swissacademy.eu
+Description: This is a wordpress implementation of a demo plugin
+Author: Marius Amza
+Version: 0.1
+Author URI: https://swissacademy.eu
+*/
